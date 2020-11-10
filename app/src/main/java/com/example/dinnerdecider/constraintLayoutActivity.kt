eb63f33bdd94c3cd3aa99fd9da673801409567ec
Yoginee Vyavhare <vyavhareyoginee4182@gmail.com>
2020-11-10 14:58:51 +0530

    phone portrait constraint layout done

---
+++ app/src/main/java/com/example/dinnerdecider/constraintLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dinnerdecider
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class constraintLayoutActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
