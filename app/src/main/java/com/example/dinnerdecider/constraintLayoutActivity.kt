a72e4bf2dce2b04bcd19c440878448e2e506d1d0
Yoginee Vyavhare <vyavhareyoginee4182@gmail.com>
2020-11-11 14:10:19 +0530

    tablet portrait, landscape view

--- app/src/main/java/com/example/dinnerdecider/constraintLayoutActivity.kt
+++ app/src/main/java/com/example/dinnerdecider/constraintLayoutActivity.kt
@@ -1,12 +1,20 @@
 package com.example.dinnerdecider
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_constraint_layout.*
 
 class constraintLayoutActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constraint_layout)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        logo.setImageDrawable(rounded)
     }
 }
